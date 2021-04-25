c3f9f26d5943800a352c7ff686fa90c2c86b67a9
mbm6415 <audrius.budrys@teliacompany.com>
2021-04-25 23:47:00 +0300

    Added Encode method

--- SimpleCipher.cs
+++ SimpleCipher.cs
@@ -6,15 +6,15 @@ public class SimpleCipher
 {
     public SimpleCipher()
     {
-       Key = RandomString();
+        Key = RandomString();
     }
 
     public SimpleCipher(string key)
     {
         Key = key;
     }
-    
-    public string Key 
+
+    public string Key
     {
         get
         {
@@ -22,9 +22,11 @@ public class SimpleCipher
         }
     }
 
+    Dictionary<char, int> alphabetElements = new Dictionary<char, int>() { { 'a', 0 }, { 'b', 1 }, { 'c', 2 }, { 'd', 3 }, { 'e', 4 }, { 'f', 5 }, { 'g', 6 }, { 'h', 7 }, { 'i', 8 }, { 'j', 9 }, { 'k', 10 }, { 'l', 11 }, { 'm', 12 }, { 'n', 13 }, { 'o', 14 }, { 'p', 15 }, { 'q', 16 }, { 'r', 17 }, { 's', 18 }, { 't', 19 }, { 'u', 20 }, { 'v', 21 }, { 'w', 22 }, { 'x', 23 }, { 'y', 24 }, { 'z', 25 } };
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
     private string RandomString()
     {
-        string alphabet = "abcdefghijklmnopqrstuvwxyz";
         var randomString = new StringBuilder();
         var random = new Random();
         for (int i = 0; i < 100; i++)
@@ -37,7 +39,23 @@ public class SimpleCipher
 
     public string Encode(string plaintext)
     {
-       
+        string cipherKey = Key;
+        while (plaintext.Length > Key.Length)
+        {
+            cipherKey += cipherKey;
+        }
+
+        var encodedString = new StringBuilder();
+        for (int i = 0; i < plaintext.Length; i++)
+        {
+            int element = alphabetElements[plaintext[i]] + alphabetElements[cipherKey[i]];
+            while (element > 25)
+            {
+                element -= 26;
+            }
+            encodedString.Append(alphabet[element]);
+        }
+        return encodedString.ToString();
     }
 
     public string Decode(string ciphertext)
